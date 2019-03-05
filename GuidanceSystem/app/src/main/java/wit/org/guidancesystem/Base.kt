3c7a3eadc95a4298a3cbc7321a875d02c41fc07d
Emma O'Brien <obrienemma0@gmail.com>
2019-03-05 14:51:04 +0000

    Implement Firebase Authentication for Logging out

--- GuidanceSystem/app/src/main/java/wit/org/guidancesystem/Base.kt
+++ GuidanceSystem/app/src/main/java/wit/org/guidancesystem/Base.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.auth.FirebaseAuth
 import org.jetbrains.anko.AnkoLogger
 import wit.org.guidancesystem.activities.Login
 
@@ -12,6 +13,7 @@ open class Base: AnkoLogger, AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when(item?.itemId){
             R.id.item_logout -> {
+                FirebaseAuth.getInstance().signOut()
                 intent = Intent(this, Login::class.java)
                 startActivity(intent)
             }
