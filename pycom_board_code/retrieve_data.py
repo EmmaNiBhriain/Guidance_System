976014a41e6cce4b6740e6a6ee5cd69441669599
Emma O'Brien <obrienemma0@gmail.com>
2019-03-23 23:16:54 +0000

    Extract the id of the bluetooth beacon associated with the destination

--- pycom_board_code/retrieve_data.py
+++ pycom_board_code/retrieve_data.py
@@ -6,3 +6,12 @@ import ufirebase as firebase
 
 URL = 'guidancesystem-f0136'  #name of database
 destination = firebase.get(URL+'/users/EMCJUDYRJKZlouOnzjGVfxpQTWR2/Destination')
+
+
+#destination = {'-LaggIDDLvglWKNRQc4J': {'xcoOrd': 6, 'bluetoothId': 'geyshx', 'name': 'garden', 'ycoOrd': 6, 'type': 'DOOR'}}
+
+value = next(iter(destination.values()))
+print(value)
+
+beaconId = value["bluetoothId"]
+print(beaconId)
