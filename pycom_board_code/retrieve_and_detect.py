7f56f4bd137e7375202a6d277041236eebb01c27
Emma O'Brien <obrienemma0@gmail.com>
2019-04-16 22:21:55 +0100

    Combine destination reading from firebase and detecting current locat…

---
+++ pycom_board_code/retrieve_and_detect.py
@@ -0,0 +1,43 @@
+import ufirebase as firebase
+import ubinascii
+import time
+from network import Bluetooth
+
+
+#################################################
+#        Retrieve Beacon ID of destination      #
+#################################################
+beaconId = ""
+
+URL = 'guidancesystem-f0136'  #name of database
+destination = firebase.get(URL+'/users/EMCJUDYRJKZlouOnzjGVfxpQTWR2/Destination')
+
+value = next(iter(destination.values()))
+print(value)
+
+beaconId = value["bluetoothId"]
+print(beaconId)
+
+
+
+if(beaconId!=""):
+    bluetooth = Bluetooth()
+    bluetooth.start_scan(20)
+    while bluetooth.isscanning():
+        adv = bluetooth.get_adv()
+        if adv:
+            # try to get the complete name
+            #print(bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL))
+
+            mfg_data = bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_MANUFACTURER_DATA)
+
+            if mfg_data:
+                # try to get the manufacturer data (Apple's iBeacon data is sent here)
+                #print(ubinascii.hexlify(mfg_data))
+                if(ubinascii.hexlify(mfg_data) == str.encode("0600010920020c3244807035e647af85fb0ed5b7eda5d114f2374d65b0")):
+                    print("you have arrived")
+                    time.sleep(10)
+                else:
+                    print(ubinascii.hexlify(mfg_data))
+else:
+    print("No beaconId")
