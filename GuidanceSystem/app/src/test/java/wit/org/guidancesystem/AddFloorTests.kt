ce0d3b86f5154f2b510b6b3b106cfd0292861abd
Emma O'Brien <obrienemma0@gmail.com>
2019-04-28 20:35:58 +0100

    Add tests to repository

---
+++ GuidanceSystem/app/src/test/java/wit/org/guidancesystem/AddFloorTests.kt
@@ -0,0 +1,26 @@
+package wit.org.guidancesystem
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Unit test for testing the RoomGraph Class
+ */
+class AddFloorTests {
+
+    //private val addFloor = addFloor()
+
+    /**
+     * Test that calculating the date for four days ago works
+     */
+    @Test
+    fun testVerifyDimensions(){
+        //val pastDate = roomGraph.getDate(4)
+        //assertEquals("24/4/2019", pastDate)
+    }
+
+
+
+
+}
---
+++ GuidanceSystem/app/src/test/java/wit/org/guidancesystem/RoomGraphTests.kt
@@ -0,0 +1,26 @@
+package wit.org.guidancesystem
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Unit test for testing the RoomGraph Class
+ */
+class RoomGraphTests {
+
+    private val roomGraph = RoomGraph()
+
+    /**
+     * Test that calculating the date for four days ago works
+     */
+    @Test
+    fun testGetDate(){
+        val pastDate = roomGraph.getDate(4)
+        assertEquals("24/4/2019", pastDate)
+    }
+
+
+
+
+}
